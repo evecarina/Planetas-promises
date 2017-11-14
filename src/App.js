d15513d586c5f310eb3e348ea6b783b1f1dd7e14
carina120794@gmail.com <you@example.com>
2017-11-14 10:33:51 -0500

    implementando la funcion SearchList

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
 import {connect} from 'redux-zero/react';
-import {search} from './actions'
+import {searchList} from './actions'
 
 const App = ({items}) =>  {
   return (
     <section>
-        <button onClick={() => search()}>Click</button>
-          <div>{ items.length!=0 ? 
-                  items[0].pl_name
-                  :
-                  <br/>
-                }</div>
-      
+        <button onClick={() => searchList()}>Click</button>
+          <div>
+            {
+              items.length!=0 ? 
+              <img src={items[0].pl_img}/>             
+                :
+              <br/>
+            }
+            {            
+            items.length!=0 ? 
+             items[0].pl_name
+               :
+             <br/>
+            }  
+            { 
+             items.length!=0 ? 
+             items[0].pl_telescope
+               :
+             <br/>
+            } 
+          </div>        
       </section>
   );
 }
--- src/actions.js
+++ src/actions.js
@@ -1,10 +1,17 @@
 import store from './store' 
-export async function search () {
-    const url = "data/Kepler-22b.json"
-    fetch(url)
+ export async function searchList(){
+     const url="data/earth-like-results.json";
+     const res1=await fetch(url);
+     const res2=await res1.json();
+     search(res2.results);
+ }
+
+export async function search (res2) {
+  console.log("res2",res2);
+    const url = res2
+    fetch(url[1])
         .then(res => res.json())
         .then(res => {
-            // console.log(res.pl_name)
             let List= [];
             List = [...store.getState().items];
             List.push(res)
@@ -16,3 +23,4 @@ export async function search () {
         })
         
 }
+
